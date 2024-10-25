1068850e8aa2f19671cd747350e8051c57b5a766
Mochamad Faizin Ahsan <mochamad.faizin@pactindo.com>
2024-10-25 09:59:10 +0700

    fix: add testing middleware_test.go

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -17,6 +20,7 @@ func Test_application_addIPToContext(t *testing.T) {
 		{"X-Forwarded-For", "192.3.2.1", "", false},
 		{"", "", "hello:world", false},
 	}
+	var app application
 	// create a dummy handler that we'll use to check the context
 	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// make sure the value exist in the context
@@ -28,7 +32,42 @@ func Test_application_addIPToContext(t *testing.T) {
 		// make sure we got a string back
 		ip, ok := val.(string)
 		if !ok {
-			t.Error(contextUserKey, "value is not a string")
+			t.Error("not string")
 		}
+		t.Log(ip)
 	})
+
+	for _, e := range tests {
+		// create the handler to test
+		handlerToTest := app.addIPToContext(nextHandler)
+
+		req := httptest.NewRequest("GET", "http://testing", nil)
+		if e.emptyAddr {
+			req.RemoteAddr = ""
+		}
+
+		if len(e.headerName) > 0 {
+			req.Header.Add(e.headerName, e.headerValue)
+		}
+		if len(e.addr) > 0 {
+			req.RemoteAddr = e.addr
+		}
+
+		handlerToTest.ServeHTTP(httptest.NewRecorder(), req)
+	}
+}
+
+func Test_application_ipFromContext(t *testing.T) {
+	// create an app var of type application
+	var app application
+	// get a context
+	ctx := context.Background()
+	// put something in the context
+	ctx = context.WithValue(ctx, contextUserKey, "test")
+	// call the function
+	ip := app.ipFromContext(ctx)
+	if !strings.EqualFold("test", ip) {
+		t.Error("wrong value returned from context")
+	}
+	t.Log(ip)
 }
