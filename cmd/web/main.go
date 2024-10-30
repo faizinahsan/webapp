0f94daced13dffa9db6a9e09062b00f5408f3177
Mochamad Faizin Ahsan <mochamad.faizin@pactindo.com>
2024-10-30 11:20:55 +0700

    feat: add more test

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"github.com/alexedwards/scs/v2"
 	"log"
 	"net/http"
@@ -8,11 +10,23 @@ import (
 
 type application struct {
 	Session *scs.SessionManager
+	DB      *sql.DB
+	DSN     string
 }
 
 func main() {
 	//set up an app config
 	app := application{}
+
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5431 user=postgres password=postgres dbname=users sslmode=disable", "Postgres connection")
+	flag.Parse()
+
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	app.DB = conn
 	//get a session manager
 	app.Session = getSession()
 	// get application routes
@@ -20,7 +34,7 @@ func main() {
 	// print out messages
 	log.Println("Starting server on port 8080...")
 	//start server
-	err := http.ListenAndServe(":8080", mux)
+	err = http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
