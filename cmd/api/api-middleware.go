f4061b4e22165fc6b31936e97908202fcc33a1e1
Mochamad Faizin Ahsan <mochamad.faizin@pactindo.com>
2024-11-08 15:07:04 +0700

    feat: add middleware and test

---
+++ cmd/api/api-middleware.go
@@ -0,0 +1,29 @@
+package main
+
+import "net/http"
+
+func (app *application) enableCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8090")
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-CSRF-Token, Authorization")
+			return
+		} else {
+			h.ServeHTTP(w, r)
+		}
+	})
+}
+
+func (app *application) authRequired(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_, _, err := app.GetTokenFromHeaderAndVerify(w, r)
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
+		return
+	})
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -22,7 +22,7 @@ func main() {
 	var app application
 	flag.StringVar(&app.Domain, "domain", "example.com", "Domain for application")
 	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5431 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5", "Posgtres connection")
-	flag.StringVar(&app.JWSecret, "jwt-secret", "secret", "signing secret")
+	flag.StringVar(&app.JWSecret, "jwt-secret", "2dce505d96a53c5768052ee90f3df2055657518dad489160df9913f66042e160", "signing secret")
 	flag.Parse()
 
 	conn, err := app.connectToDB()
